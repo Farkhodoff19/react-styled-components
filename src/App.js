357a7ed17005ee1cdaa467ea85bd6d8e905bb647
Farkhodoff19 <farkhodoffcodes@gmail.com>
2023-02-24 19:37:24 +0300

    react-styled-components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./Login/Login";
+import Reagister from "./Register/Reagister";
+
+export default function App() {
+  return (
+    <div>
+      <Login />
+      <Reagister />
+    </div>
+  );
+}
---
+++ src/Register/Reagister.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Reagister() {
+  return <div>Reagister</div>;
+}
